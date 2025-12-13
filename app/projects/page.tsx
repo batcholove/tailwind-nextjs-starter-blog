d79117734679147af1211348f84eca4e4699595c
Ryan Batch <rbatch@Ryans-MacBook-Pro-2.local>
2025-12-12 18:03:17 -0700

    Update Projects page lead text

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            A collection of things I've designed and learn from over the course of my career.
           </p>
         </div>
         <div className="container py-12">
