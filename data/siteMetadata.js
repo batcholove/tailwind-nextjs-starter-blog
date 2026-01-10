fd92d235528564b058dfc8485badd43fe13ce29b
Ryan Batch <rbatch@Ryans-MacBook-Pro-2.local>
2026-01-09 19:44:09 -0700

    Update social banner to use og-image.png

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
-  socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
+  socialBanner: `${process.env.BASE_PATH || ''}/static/images/og-image.png`,
   // mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'ryan@parallelarts.com',
   github: 'https://github.com',
