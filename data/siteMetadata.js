d6c78f36ff45769fed738c2da15c34d48a8aff6b
Ryan Batch <rbatch@Ryans-MacBook-Pro-2.local>
2026-01-12 16:48:12 -0700

    Fix staging siteUrl to modnetic.com

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,7 +6,7 @@ const siteMetadata = {
   description: 'A portfolio site showcasing my design projects and case studies.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://ryanbatch.com',
+  siteUrl: 'https://modnetic.com',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: '/static/images/og-image.png',
